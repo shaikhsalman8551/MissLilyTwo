b09d812f6ddc7efbe63a010aa976ddf0a4f10e1f
shaikhsalman8551 <shaikhsalman8551@gmail.com>
2026-02-08 07:43:24 +0530

    new

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { FaTshirt } from 'react-icons/fa';
-if (localStorage.getItem('adminLoggedIn')) {
+
+const Footer = () => {
+  if (localStorage.getItem('adminLoggedIn')) {
     return null;
   }
-const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white mt-20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
